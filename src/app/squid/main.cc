eb316f8a86f266af687dee9d5b949101caa6ece8
Rumen <rumenmitov@protonmail.com>
2024-09-02 18:27:14 +0200

    ahci attempt

--- src/app/squid/main.cc
+++ src/app/squid/main.cc
@@ -11,8 +11,8 @@ void Component::construct(Genode::Env &env) {
 //    vm_page hello;
 //    vm_page *hello_ptr = &hello;
 //    hello_ptr->id = 23;
-    char hello_ptr[] = "hello world!";
-    main._new_file("/dev/hdd/squid-cache/hello", (void*) hello_ptr, sizeof(hello_ptr));
+//    char hello_ptr[] = "hello world!";
+//    main._new_file("/dev/hdd/squid-cache/hello", (void*) hello_ptr, sizeof(hello_ptr));
     main._read_file("/dev/hdd/squid-cache/hello");
     Genode::log("passed.");
 }
