7705e0f2cb544158abe1b02baffae843ca1e556f
Rumen <rumenmitov@protonmail.com>
2024-09-02 18:08:44 +0200

    updating to genode-24.08

---
+++ src/app/squid/target.mk
@@ -0,0 +1,17 @@
+TARGET   = squid
+SRC_CC   = main.cc squid.cc
+LIBS     = lwext4 base format vfs
+
+INC_DIR += $(call select_from_ports,lwext4)/include
+INC_DIR += $(REP_DIR)/src/lib/lwext4/include
+
+CC_OPT += -DCONFIG_USE_DEFAULT_CFG=1
+CC_OPT += -DCONFIG_HAVE_OWN_ERRNO=1
+CC_OPT += -DCONFIG_HAVE_OWN_ASSERT=1
+CC_OPT += -DCONFIG_BLOCK_DEV_CACHE_SIZE=256
+
+vpath     %.c $(LWEXT4_DIR)/src
+vpath     %.c $(LWEXT4_DIR)/blockdev
+vpath qsort.c $(REP_DIR)/src/lib/lwext4/
+vpath    %.cc $(REP_DIR)/src/lib/lwext4/
+
