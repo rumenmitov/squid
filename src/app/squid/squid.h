533c6db2f3522542f4761974e97d6fbaa7f785a0
Rumen <rumenmitov@protonmail.com>
2024-09-07 14:21:27 +0200

    fixing merge

--- src/app/squid/squid.h
+++ src/app/squid/squid.h
@@ -2,33 +2,6 @@
  * @Author Rumen Mitov
  * @Date 2024-09-07
 
-<<<<<<< HEAD
-#include <base/component.h>
-#include <base/sleep.h>
-#include <base/attached_rom_dataspace.h>
-#include <base/heap.h>
-#include <os/vfs.h>
-#include <vfs/file_system_factory.h>
-#include <base/buffered_output.h>
-#include <timer_session/connection.h>
-
-
-/*
- * Squid Structure is as follows:
- * Upper Directory - designated by first two chars of hash
- * Middle Directory - designated by second pair chars of hash
- * Lower Directory - designated by third pair chars of hash
- * Squid files - files that contain virtual page information
- */
-
-#define PAGE_SIZE INT_MAX
-#define MD5_HASH_LEN 32
-
-namespace Squid_snapshot {
-    using namespace Genode;
-    struct Main;
-}
-=======
  squid.h provides an API to snapshot data to the disk.
  It is organized as a Radix Tree with an Upper Directory, Middle Directory, and a
  Lower Directory.
@@ -42,7 +15,6 @@ namespace Squid_snapshot {
 
 #ifndef __SQUID_H
 #define __SQUID_H
->>>>>>> unit_tests
 
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -64,16 +36,10 @@ typedef enum SquidError {
 
 // NOTE: This is only here for testing purposes.
 typedef struct vm_page {
-    char vpid[32]; // virtual page identification
+    int id;
 } vm_page;
 
-<<<<<<< HEAD
 
-struct Squid_snapshot::Main
-{
-    Env &_env;
-    Heap _heap { _env.ram(), _env.rm() };
-=======
 /**
  * @brief Creates squid file.
  * @param const char *
@@ -100,21 +66,4 @@ SquidError_t squid_file_read(char const *path, void *payload);
 SquidError_t squid_file_delete(char const *path);
 
 
-<<<<<<< HEAD
-	Vfs::Simple_env _vfs_env { _env, _heap, _config.xml().sub_node("vfs") };
-
-	typedef Directory::Path Path;
-
-	Directory _root_dir { _vfs_env };
-
-    Timer::Connection _timer { _env };
-
-    char* gen_hash();
-
-	void init_squid_file(Path const path, vm_page *me, size_t size);
-	void read_squid_file(Path const path, vm_page *me);
-	void delete_squid_file(Path const path);
-};
-=======
 #endif // __SQUID_H
->>>>>>> unit_tests
